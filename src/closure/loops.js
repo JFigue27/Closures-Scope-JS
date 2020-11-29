9f2c1de6f7a1cb07f8d7522d6f478242ab0b9efa
Jose Jaime Figueroa Barrios <jaime.figueroa@inspiracode.net>
2020-11-29 09:51:11 -0600

    Debuggin and Hoisting

---
+++ src/closure/loops.js
@@ -0,0 +1,15 @@
+const anotherFunction = () => {
+  //
+  for (let i = 0; i < 10; i++) {
+    setTimeout(() => {
+      console.log(i);
+    }, 500);
+  }
+};
+
+anotherFunction();
+
+const fruits = () => {
+  var fruit = "apple";
+  console.log(fruit);
+};
---
+++ src/hoisting/index.js
@@ -0,0 +1,9 @@
+a = 2;
+var a = 2;
+console.log(a);
+
+function nameOfDog(name) {
+  console.log(name);
+}
+
+nameOfDog("Sparky");
